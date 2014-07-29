cd47d61e921cb41f30ae81acb045ccfd94807a18
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-07-29 14:28:11 +0100

    Initial Commit

---
+++ completionClosure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  completionClosure
+//
+//  Created by Gabriel Massana on 29/07/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
